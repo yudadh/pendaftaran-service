c6ebf2f9a441aa2a9fbd7d13dea5a1a244feeec7
YudaDh <yudadarmawan26@gmail.com>
2025-06-22 22:11:16 +0800

    fix jwt utils

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,12 +2,21 @@ import jwt from 'jsonwebtoken';
 import { env } from '../config/envConfig';
 import { JwtPayloadToken } from '../interfaces/pendaftaranInterface';
 
+type StringValue =
+  | `${number}ms`
+  | `${number}s`
+  | `${number}m`
+  | `${number}h`
+  | `${number}d`
+  | `${number}w`
+  | `${number}y`;
+
 export const generateAccessToken = (payload: JwtPayloadToken) => {
-  return jwt.sign(payload, env.JWT_SECRET, { expiresIn: env.JWT_ACCESS_EXPIRY });
+  return jwt.sign(payload, env.JWT_SECRET, { expiresIn: env.JWT_ACCESS_EXPIRY as StringValue });
 };
 
 export const generateRefreshToken = (payload: JwtPayloadToken) => {
-  return jwt.sign(payload, env.JWT_SECRET, { expiresIn: env.JWT_REFRESH_EXPIRY });
+  return jwt.sign(payload, env.JWT_SECRET, { expiresIn: env.JWT_REFRESH_EXPIRY as StringValue });
 };
 
 export const verifyToken = (token: string) => {
